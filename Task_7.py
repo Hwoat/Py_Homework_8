607e4053519a440137c98e340a2aebea51c1c513
Hwoat <45818542+Hwoat@users.noreply.github.com>
2020-07-27 19:42:44 +0300

    Add files via upload

---
+++ Task_7.py
@@ -0,0 +1,34 @@
+'''
+Реализовать проект «Операции с комплексными числами». Создайте класс «Комплексное число»,
+реализуйте перегрузку методов сложения и умножения комплексных чисел.
+Проверьте работу проекта, создав экземпляры класса (комплексные числа) и
+выполнив сложение и умножение созданных экземпляров.
+Проверьте корректность полученного результата.
+'''
+
+#http://www.math24.ru/%D0%BA%D0%BE%D0%BC%D0%BF%D0%BB%D0%B5%D0%BA%D1%81%D0%BD%D1%8B%D0%B5-%D1%87%D0%B8%D1%81%D0%BB%D0%B0.html
+#https://www.webmath.ru/poleznoe/formules_16_1.php
+
+class ComplexNum:
+    def __init__(self, a, b, *args):
+        self.a = a
+        self.b = b
+        self.z = 'a + b * i'
+
+    def __add__(self, other):
+        print(f'Сумма comp_1 и comp_2 равна')
+        return f'z = {self.a + other.a} + {self.b + other.b} * i'
+
+    def __mul__(self, other):
+        print(f'Произведение comp_1 и comp_2 равно')
+        return f'z = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
+
+    def __str__(self):
+        return f'z = {self.a} + {self.b} * i'
+
+
+comp_1 = ComplexNum(1, -2)
+comp_2 = ComplexNum(3, 4)
+print(comp_1)
+print(comp_1 + comp_2)
+print(comp_1 * comp_2)
